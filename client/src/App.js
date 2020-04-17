7b58b078e35ebe98a79c5f47406b42a35d73839f
Arpedia <plane629@live.jp>
2020-04-17 16:09:21 +0900

    can opne side menu area

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
+import { OffCanvas, OffCanvasMenu, OffCanvasBody } from "react-offcanvas";
 import './App.css';
 import Map from './components/Map.js';
 import Coodinate from './components/Coordinate.js';
 
+
 class App extends Component{
   constructor(props){
     super(props);
-    this.state = { "feature": null };
+    this.state = { 
+      "feature": null,
+      "isMenuOpened": false,
+    };
   }
 
   selectFeature(feature){
     if (JSON.stringify(this.state.feature) !== JSON.stringify(feature)){
       this.setState({ "feature": feature });
     }
+
+    if (this.state.feature){
+      this.handleClick(true);
+    }
+  }
+  
+  handleClick(doopen) {
+    // toggles the menu opened state
+    this.setState({ isMenuOpened: doopen });
   }
 
   render() {
     return (
       <div className="App">
-        <Coodinate feature={this.state.feature} />
-        <Map click={(v) => this.selectFeature(v)} />
+        <OffCanvas
+          width={600}
+          transitionDuration={300}
+          effect={"parallax"}
+          isMenuOpened={this.state.isMenuOpened}
+          position={"right"}
+        >
+
+          <OffCanvasBody className={"canvas-body"}>
+            <Coodinate feature={this.state.feature} />
+            <Map click={(v) => this.selectFeature(v)} />
+          </OffCanvasBody>
+
+          <OffCanvasMenu className={"canvas-menu"} width={700}>
+          </OffCanvasMenu>
+        </OffCanvas>
       </div>
     );
   }
